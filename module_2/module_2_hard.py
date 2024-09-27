99b4c7757d88f9b6de0cdbc5f82a1b8fb5218ec1
Project743 <chip441988@gmail.com>
2024-09-27 18:18:04 +0200

    обновлено модуль 2_hard

--- module_2/module_2_hard.py
+++ module_2/module_2_hard.py
@@ -1,9 +1,15 @@
 number = int(input('Введите заданое число (от 3 до 20): '))
-result = ''
-for i in range(1, number):
-    for j in range(i + 1, number):
-        if (i + j) > number:
-            break
-        if number % (i + j) == 0:
-            result += str(i) + str(j)
-print(result)
+
+def get_result(numder):
+    result = ''
+    for i in range(1, number):
+        for j in range(i + 1, number):
+            if (i + j) > number:
+                break
+            if number % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+if number>=3 and number<=20:
+    print(get_result(number))
+else:
+    print('Число выходит за указанные границы!')
