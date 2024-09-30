63c7bca171d7335d4133bae15a9c109fadb8f84b
Project743 <chip441988@gmail.com>
2024-09-30 15:00:50 +0200

    выполнить модуль 3_2

---
+++ module_3/module_3_2.py
@@ -0,0 +1,24 @@
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    if validation_email(recipient) == False or validation_email(sender) == False:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+
+def validation_email(email):
+    if '@' not in email:
+        return False
+    elif email[-4:] == '.com' or email[-4:] == '.net' or email[-3:] == '.ru':
+        return True
+    else:
+        return False
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
