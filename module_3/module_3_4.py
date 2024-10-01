603dd99f9d6f44bf35e286b7178560345ff0e2a8
Project743 <chip441988@gmail.com>
2024-10-01 12:23:01 +0200

    выполнить модуль 3_4

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -5,6 +5,7 @@ def single_root_words(root_words, *other_words):
             same_words.append(other_words[i])
     return same_words
 
+
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
