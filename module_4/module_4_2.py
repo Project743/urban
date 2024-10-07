f0348ad8ee193d81273f358a96d555533d97cf50
Project <chip441988@gmail.com>
2024-10-07 13:49:48 +0200

    выполнить module_4_2

---
+++ module_4/module_4_2.py
@@ -0,0 +1,9 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+test_function()
+# inner_function() данная функция определена внутри функции test_function и не может быть извана из вне
