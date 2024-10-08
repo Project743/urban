cd32f86aafb7d6605ed7d4d15884699c92a5e325
Project <chip441988@gmail.com>
2024-10-08 16:23:22 +0200

    выполнить module_5_1

---
+++ module_5/module_5_1.py
@@ -0,0 +1,19 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floor:
+            print('Такого этажа не сушествует.')
+        else:
+            count = 1
+            while new_floor >= count:
+                print(f'Вы поднялись на {count} этаж')
+                count += 1
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
