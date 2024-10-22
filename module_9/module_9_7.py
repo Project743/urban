230fc2c390a1c24c76aa47554d35ccd0f346a2c9
Project <chip441988@gmail.com>
2024-10-22 21:50:02 +0200

    выполнить module_9_7

---
+++ module_9/module_9_7.py
@@ -0,0 +1,21 @@
+def is_prime(func):
+    def wraper(*args):
+        res = func(*args)
+        bool_1 = [x for x in range(2, int(res ** 0.5) + 1) if res % x == 0]
+        if bool_1:
+            print('Составное')
+        else:
+            print('Простое')
+
+        return res
+
+    return wraper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+if __name__ == '__main__':
+    result = sum_three(2, 3, 6)
+    print(result)
