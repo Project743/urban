67d4f3c113a1b1551f96f5e886e630c0d1a8acff
Project <chip441988@gmail.com>
2024-10-23 16:33:21 +0200

    выполнить module_10_2

---
+++ module_10/module_10_2.py
@@ -0,0 +1,37 @@
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        # self.name_k = name
+        self.power = power
+        self.enemy = 100
+        super().__init__(name=name)
+
+    def run(self):
+        count_day = 0
+        print(f'{self.name}, на нас напали')
+        while self.enemy > 0:
+            count_day += 1
+            self.enemy -= self.power
+            print(f'{self.name} сражается {count_day} дней, осталось {self.enemy} войнов')
+            sleep(1)
+        print(f'{self.name} одержал победу спустя {count_day}дней')
+
+
+if __name__ == '__main__':
+    first_knight = Knight('Sir Lancelot', 10)
+    second_knight = Knight("Sir Galahad", 20)
+    list_knight = [first_knight, second_knight]
+
+    threads = []
+    for knight in list_knight:
+        thread = knight
+        sleep(0.01)  # задержка что бы разные потоки не выводили информацию в одну строку
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+    print('Все битвы закончились!')
