97ec2046ad19d753096839f4f71edc14696dd4cb
Project743 <chip441988@gmail.com>
2024-11-02 16:14:36 +0200

    update module_11_2

--- module_11/module_11_2.py
+++ module_11/module_11_2.py
@@ -1,11 +1,10 @@
 def introspection_info(obj):
     dict_data = {'type': type(obj).__name__}
-    if hasattr(obj, '__module__'):
-        dict_data['module'] = getattr(obj, '__module__')
+    dict_data['module'] = obj.__class__.__module__
     dict_data['attributes'] = [attr for attr in dir(obj) if
-                               not callable(getattr(obj, attr)) and not attr.startswith('__')]
+                               not callable(getattr(obj, attr))]
     dict_data['methods'] = [method for method in dir(obj) if
-                            callable(getattr(obj, method)) and not method.startswith('__')]
+                            callable(getattr(obj, method))]
     return dict_data
 
 
