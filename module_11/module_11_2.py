033f4ffec06de8dd3ac74370007c6e92cfc26c24
Project <chip441988@gmail.com>
2024-11-02 12:36:15 +0200

    выполнить модуль 11_2

--- module_11/module_11_2.py
+++ module_11/module_11_2.py
@@ -1,12 +1,13 @@
-
 def introspection_info(obj):
-    dict_data = {}
-    dict_data['type'] = type(obj).__name__
-    if hasattr(obj,'__module__'):
-        dict_data['module'] = getattr(obj,'__module__')
-    dict_data['attributes'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')]
-    dict_data['methods'] = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith('__')]
+    dict_data = {'type': type(obj).__name__}
+    if hasattr(obj, '__module__'):
+        dict_data['module'] = getattr(obj, '__module__')
+    dict_data['attributes'] = [attr for attr in dir(obj) if
+                               not callable(getattr(obj, attr)) and not attr.startswith('__')]
+    dict_data['methods'] = [method for method in dir(obj) if
+                            callable(getattr(obj, method)) and not method.startswith('__')]
     return dict_data
+
 
 class House:
     def __init__(self, name, number_of_floors):
@@ -15,7 +16,7 @@ class House:
 
     def go_to(self, new_floors):
         if new_floors not in range(1, self.number_of_floors + 1):
-            print('Такого этажа не сушествует.')
+            print('Такого этажа не существует.')
         else:
             count = 1
             while new_floors >= count:
@@ -28,4 +29,3 @@ h1 = House('ЖК Горский', 18)
 s = 'lst'
 number_info = introspection_info(h1)
 print(number_info)
-
