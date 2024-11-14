8848a856a0f6207750c2745d2584437113a5ac95
Project743 <chip441988@gmail.com>
2024-11-14 10:53:42 +0200

    выполнить module_0_1

---
+++ module_0/module_0_1.py
@@ -0,0 +1,10 @@
+# task1
+print(9 ** 0.5 * 5)
+# task2
+print(9.99 > 9.98 and 1000 != 1000.1)
+# task3
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+# task4
+print(int(float('123.456')*10)%10)
